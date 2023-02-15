e6e902b4aea862c1777d1c265b0051f8f53be1ff
RemHero <1104304963@qq.com>
2023-02-15 21:18:48 +0800

    7

--- .github/influxdb.sh
+++ .github/influxdb.sh
@@ -10,7 +10,7 @@ sh -c "ls influxdb2-2.0.7-linux-amd64"
 
 sudo sh -c "cd influxdb2-2.0.7-linux-amd64/; nohup ./influxd run --bolt-path=~/.influxdbv2/influxd.bolt --engine-path=~/.influxdbv2/engine --http-bind-address=:8086 --query-memory-bytes=20971520 &"
 
-sh -c "sleep 30"
+sh -c "sleep 3"
 
 sh -c "./influxdb2-2.0.7-linux-amd64/influx setup --org testOrg --bucket testBucket --username user --password 12345678 --token testToken --force"
 
