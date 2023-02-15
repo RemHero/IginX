fa7e4a3b19d2c7944ef72a566ecbfd9908822e57
RemHero <1104304963@qq.com>
2023-02-15 09:33:49 +0800

    1

--- .github/testUnion.sh
+++ .github/testUnion.sh
@@ -9,18 +9,6 @@ if [ $? -ne 0 ];then
 	exit 1
 else
 	echo " make  -- Success !"
-	exit 0
 fi
-
-result = $?
-echo "?????????????" $?
 
 cd /home/runner/work/IGinX/IGinX/test
-
-if [ result -ne 0 ];then
-	echo " make  -- Faile  : "$?
-	exit 1
-else
-	echo " make  -- Success !"
-	exit 0
-fi
