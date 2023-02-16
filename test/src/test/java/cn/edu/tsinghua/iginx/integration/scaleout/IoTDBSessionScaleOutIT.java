dae25539fc9c6fcdccc6dd4c5b2f7186e2f609d9
RemHero <80055724+RemHero@users.noreply.github.com>
2023-02-16 16:35:36 +0800

    ce

--- test/src/test/java/cn/edu/tsinghua/iginx/integration/scaleout/IoTDBSessionScaleOutIT.java
+++ test/src/test/java/cn/edu/tsinghua/iginx/integration/scaleout/IoTDBSessionScaleOutIT.java
@@ -31,28 +31,28 @@ public class IoTDBSessionScaleOutIT extends BaseSessionIT implements IoTDBBaseSc
     @Test
     public void oriHasDataExpHasDataIT() throws Exception {
         DBConf();
-        BaseSessionIT.session.executeSql("ADD STORAGEENGINE (\"127.0.0.1\", 6668, \"" + storageEngineType + "\", \"username:root, password:root, sessionPoolSize:20, has_data:true, is_read_only:true\");");
+        //BaseSessionIT.session.executeSql("ADD STORAGEENGINE (\"127.0.0.1\", 6668, \"" + storageEngineType + "\", \"username:root, password:root, sessionPoolSize:20, has_data:true, is_read_only:true\");");
         capacityExpansion();
     }
 
     @Test
     public void oriHasDataExpNoDataIT() throws Exception {
         DBConf();
-        BaseSessionIT.session.executeSql("ADD STORAGEENGINE (\"127.0.0.1\", 6668, \"" + storageEngineType + "\", \"username:root, password:root, sessionPoolSize:20, has_data:no, is_read_only:true\");");
+        //BaseSessionIT.session.executeSql("ADD STORAGEENGINE (\"127.0.0.1\", 6668, \"" + storageEngineType + "\", \"username:root, password:root, sessionPoolSize:20, has_data:no, is_read_only:true\");");
         capacityExpansion();
     }
 
     @Test
     public void oriNoDataExpHasDataIT() throws Exception {
         DBConf();
-        BaseSessionIT.session.executeSql("ADD STORAGEENGINE (\"127.0.0.1\", 6668, \"" + storageEngineType + "\", \"username:root, password:root, sessionPoolSize:20, has_data:true, is_read_only:true\");");
+        //BaseSessionIT.session.executeSql("ADD STORAGEENGINE (\"127.0.0.1\", 6668, \"" + storageEngineType + "\", \"username:root, password:root, sessionPoolSize:20, has_data:true, is_read_only:true\");");
         capacityExpansion();
     }
 
     @Test
     public void oriNoDataExpNoDataIT() throws Exception {
         DBConf();
-        BaseSessionIT.session.executeSql("ADD STORAGEENGINE (\"127.0.0.1\", 6668, \"" + storageEngineType + "\", \"username:root, password:root, sessionPoolSize:20, has_data:no, is_read_only:true\");");
+       // BaseSessionIT.session.executeSql("ADD STORAGEENGINE (\"127.0.0.1\", 6668, \"" + storageEngineType + "\", \"username:root, password:root, sessionPoolSize:20, has_data:no, is_read_only:true\");");
         capacityExpansion();
     }
 
