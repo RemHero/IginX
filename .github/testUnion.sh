c25f7740538d9f3ef58e97ddc0851e7d95e012bf
RemHero <1104304963@qq.com>
2023-02-17 09:38:40 +0800

    uuuu

--- .github/testUnion.sh
+++ .github/testUnion.sh
@@ -4,21 +4,27 @@ pwd
 
 cd ..
 
-FILE = cat ./test/src/test/java/cn/edu/tsinghua/iginx/integration/testControler/testTask.txt
-LISTS=($FILE)
+i=0
+
+while read line
+do
+  LISTS[$i]=$line
+  let i+=1
+done <  ./test/src/test/java/cn/edu/tsinghua/iginx/integration/testControler/testTask.txt
+
 echo "test IT name list : "${LISTS[*]}
 
-#for line in ${LISTS[@]}
-#do
-#   echo "test IT name : "$line
-#   mvn test -q -Dtest=$line -DfailIfNoTests=false
-#
-#   if [ $? -ne 0 ];then
-#     echo " test  -- Faile  : "$?
-#     exit 1
-#   else
-#     echo " test  -- Success !"
-#   fi
-#done
+for line in ${LISTS[@]}
+do
+   echo "test IT name : "$line
+   mvn test -q -Dtest=$line -DfailIfNoTests=false
+
+   if [ $? -ne 0 ];then
+     echo " test  -- Faile  : "$?
+     exit 1
+   else
+     echo " test  -- Success !"
+   fi
+done
 
 cd test
