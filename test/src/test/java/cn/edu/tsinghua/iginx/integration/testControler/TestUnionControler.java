0420608e80e07743f2671f68b8745ce1304201d6
RemHero <1104304963@qq.com>
2023-02-17 11:15:51 +0800

    ssss

--- test/src/test/java/cn/edu/tsinghua/iginx/integration/testControler/TestUnionControler.java
+++ test/src/test/java/cn/edu/tsinghua/iginx/integration/testControler/TestUnionControler.java
@@ -125,7 +125,7 @@ public class TestUnionControler {
 //            else
 //                in = p.getInputStream();
 
-            ProcessBuilder builder = new ProcessBuilder("bash " + command);
+            ProcessBuilder builder = new ProcessBuilder(Arrays.asList("bash ", command));
             builder.redirectErrorStream(true);
             p = builder.start();
             BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
