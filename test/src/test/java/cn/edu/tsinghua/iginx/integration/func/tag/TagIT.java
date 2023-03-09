b32ebad7f373c87ef1403714d67244fe191524ac
RemHero <1104304963@qq.com>
2023-03-09 10:21:10 +0800

    fix the DBConf

--- test/src/test/java/cn/edu/tsinghua/iginx/integration/func/tag/TagIT.java
+++ test/src/test/java/cn/edu/tsinghua/iginx/integration/func/tag/TagIT.java
@@ -26,7 +26,7 @@ public class TagIT {
 
     public TagIT() throws IOException {
         ConfLoder conf = new ConfLoder(Controller.CONFIG_FILE);
-        DBConf dbConf = conf.loadDBConf();
+        DBConf dbConf = conf.loadDBConf(conf.getStorageType());
         this.ifClearData = dbConf.getEnumValue(DBConf.DBConfType.isAbleToClearData);
         this.isAbleToDelete = dbConf.getEnumValue(DBConf.DBConfType.isAbleToDelete);
         this.ifScaleOutIn = conf.getStorageType() != null;
