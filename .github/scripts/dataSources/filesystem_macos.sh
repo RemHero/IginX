7b0d18f5b494d29921a2abb3ced812e7674c3012
RemHero <1104304963@qq.com>
2023-09-20 09:37:30 +0800

    test

--- .github/scripts/dataSources/filesystem_macos.sh
+++ .github/scripts/dataSources/filesystem_macos.sh
@@ -2,18 +2,18 @@
 
 set -e
 
-sed -i "" "s/storageEngineList=127.0.0.1#6667#iotdb12/#storageEngineList=127.0.0.1#6667#iotdb12/" conf/config.properties
+sed -i "" "s/storageEngineList=127.0.0.1#6667#iotdb12/#storageEngineList=127.0.0.1#6667#iotdb12/" core/target/iginx-core-0.6.0-SNAPSHOT/conf/config.properties
 
-sed -i "" "s/#storageEngineList=127.0.0.1#6667#filesystem/storageEngineList=127.0.0.1#$1#filesystem/" conf/config.properties
+sed -i "" "s/#storageEngineList=127.0.0.1#6667#filesystem/storageEngineList=127.0.0.1#$1#filesystem/" core/target/iginx-core-0.6.0-SNAPSHOT/conf/config.properties
 
-sed -i "" "s/#iginx_port=6888#/#iginx_port=$2#/g" conf/config.properties
+sed -i "" "s/#iginx_port=6888#/#iginx_port=$2#/g" core/target/iginx-core-0.6.0-SNAPSHOT/conf/config.properties
 
-sed -i "" "s/enablePushDown=true/enablePushDown=false/" conf/config.properties
+sed -i "" "s/enablePushDown=true/enablePushDown=false/" core/target/iginx-core-0.6.0-SNAPSHOT/conf/config.properties
 
-sed -i "" "s#dummy_dir=/path/to/your/data#dummy_dir=$3#g" conf/config.properties
+sed -i "" "s#dummy_dir=/path/to/your/data#dummy_dir=$3#g" core/target/iginx-core-0.6.0-SNAPSHOT/conf/config.properties
 
-sed -i "" "s#dir=/path/to/your/filesystem#dir=$4#g" conf/config.properties
+sed -i "" "s#dir=/path/to/your/filesystem#dir=$4#g" core/target/iginx-core-0.6.0-SNAPSHOT/conf/config.properties
 
-sed -i "" "s/#has_data=false#/#has_data=$5#/g" conf/config.properties
+sed -i "" "s/#has_data=false#/#has_data=$5#/g" core/target/iginx-core-0.6.0-SNAPSHOT/conf/config.properties
 
-sed -i "" "s/#is_read_only=false/#is_read_only=$6/g" conf/config.properties
+sed -i "" "s/#is_read_only=false/#is_read_only=$6/g" core/target/iginx-core-0.6.0-SNAPSHOT/conf/config.properties
