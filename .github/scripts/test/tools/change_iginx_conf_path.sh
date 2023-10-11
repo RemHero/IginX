1213a1b8f1fcab1eab73eb91d9b1ca786b36da8b
RemHero <1104304963@qq.com>
2023-10-11 13:34:18 +0800

    tmp

--- .github/scripts/test/tools/change_iginx_conf_path.sh
+++ .github/scripts/test/tools/change_iginx_conf_path.sh
@@ -2,6 +2,8 @@
 
 set -e
 
-cp -f core/target/iginx-core-*-SNAPSHOT/sbin/start_iginx.sh core/target/iginx-core-*-SNAPSHOT/sbin/start_iginx.sh.bak
+#cp -f core/target/iginx-core-*-SNAPSHOT/sbin/start_iginx.sh core/target/iginx-core-*-SNAPSHOT/sbin/start_iginx.sh.bak
+
+pwd
 
 sed -i "s#/conf/config.properties#$1#g" core/target/iginx-core-*-SNAPSHOT/sbin/start_iginx.sh
