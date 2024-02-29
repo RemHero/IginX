d93bacf3b3980b36893a4718dfcc684f2c613182
RemHero <1104304963@qq.com>
2024-02-29 10:13:24 +0800

    test

--- test/src/test/java/cn/edu/tsinghua/iginx/integration/expansion/parquet/ParquetHistoryDataGenerator.java
+++ test/src/test/java/cn/edu/tsinghua/iginx/integration/expansion/parquet/ParquetHistoryDataGenerator.java
@@ -39,10 +39,10 @@ public class ParquetHistoryDataGenerator extends BaseHistoryDataGenerator {
       List<List<Object>> valuesList) {
     LOGGER.info("start writing data, dir:{}, filename:{}.", dir, filename);
 
-    if (!PARQUET_PARAMS.containsKey(port)) {
-      LOGGER.error("writing to unknown port {}.", port);
-      return;
-    }
+//    if (!PARQUET_PARAMS.containsKey(port)) {
+//      LOGGER.error("writing to unknown port {}.", port);
+//      return;
+//    }
 
       LOGGER.info("check");
     Path dirPath = Paths.get("../" + dir);
