36cd439b4ea78efbbca0181e29446d5158c6b540
RemHero <1104304963@qq.com>
2024-04-26 11:39:54 +0800

    检测docker mysql运行环境

---
+++ .github/scripts/test/tools/wait_for_health.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+container_name="$1"
+
+if [ -z "$container_name" ]; then
+    echo "Usage: $0 <container_name>"
+    exit 1
+fi
+
+while true; do
+    health_status=$(docker inspect --format='{{json .State.Health.Status}}' "$container_name")
+
+    if [[ $health_status == '"healthy"' ]]; then
+        echo "Container $container_name is now healthy."
+        break
+    else
+        echo "Container $container_name is still not healthy. Waiting..."
+        sleep 5
+    fi
+done
