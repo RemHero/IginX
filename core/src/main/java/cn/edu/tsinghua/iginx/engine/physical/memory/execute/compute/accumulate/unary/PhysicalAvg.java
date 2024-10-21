424bbdac516910cf81dfe2e07c2c2f7ddf7e3e56
RemHero <1104304963@qq.com>
2024-10-21 13:27:13 +0800

    add agg func v2.0

--- core/src/main/java/cn/edu/tsinghua/iginx/engine/physical/memory/execute/compute/accumulate/unary/PhysicalAvg.java
+++ core/src/main/java/cn/edu/tsinghua/iginx/engine/physical/memory/execute/compute/accumulate/unary/PhysicalAvg.java
@@ -10,167 +10,167 @@ import org.apache.arrow.vector.types.pojo.Field;
 
 public class PhysicalAvg extends UnaryAccumulator {
 
-    public static final String NAME = "avg";
+  public static final String NAME = "avg";
 
-    public PhysicalAvg() {
-        super(NAME);
+  public PhysicalAvg() {
+    super(NAME);
+  }
+
+  @Override
+  protected UnaryState initializeImpl(BufferAllocator allocator, Field field)
+      throws ComputeException {
+    Types.MinorType minorType = Types.getMinorTypeForArrowType(field.getType());
+    switch (minorType) {
+      case INT:
+        return new IntAvgState(allocator, field, this);
+      case BIGINT:
+        return new BigIntAvgState(allocator, field, this);
+      case FLOAT4:
+        return new Float4AvgState(allocator, field, this);
+      case FLOAT8:
+        return new Float8AvgState(allocator, field, this);
+      default:
+        throw new NotAllowArgumentTypeException(this, 0, minorType);
+    }
+  }
+
+  protected abstract static class AvgState extends UnaryState {
+
+    protected long count; // Number of non-null elements
+
+    protected AvgState(BufferAllocator allocator, Field field, PhysicalAvg accumulator) {
+      super(allocator, field, accumulator);
+      this.count = 0;
     }
 
     @Override
-    protected UnaryState initializeImpl(BufferAllocator allocator, Field field)
-        throws ComputeException {
-        Types.MinorType minorType = Types.getMinorTypeForArrowType(field.getType());
-        switch (minorType) {
-            case INT:
-                return new IntAvgState(allocator, field, this);
-            case BIGINT:
-                return new BigIntAvgState(allocator, field, this);
-            case FLOAT4:
-                return new Float4AvgState(allocator, field, this);
-            case FLOAT8:
-                return new Float8AvgState(allocator, field, this);
-            default:
-                throw new NotAllowArgumentTypeException(this, 0, minorType);
-        }
+    public boolean needMoreData() throws ComputeException {
+      return true;
     }
 
-    protected abstract static class AvgState extends UnaryState {
+    @Override
+    public void close() throws ComputeException {}
 
-        protected long count; // Number of non-null elements
-
-        protected AvgState(BufferAllocator allocator, Field field, PhysicalAvg accumulator) {
-            super(allocator, field, accumulator);
-            this.count = 0;
-        }
-
-        @Override
-        public boolean needMoreData() throws ComputeException {
-            return true;
-        }
-
-        @Override
-        public void close() throws ComputeException {}
-
-        @Override
-        protected FieldVector evaluateImpl() throws ComputeException {
-            if (count == 0) {
-                FieldVector result = field.createVector(allocator);
-                result.setNull(0); // If there's no data, the average is null
-                return result;
-            }
-            FieldVector result = field.createVector(allocator);
-            writeValue(result.getMinorType().getNewFieldWriter(result));
-            return result;
-        }
-
-        protected abstract double getSum();
-
-        protected void writeValue(FieldWriter writer) {
-            if (count > 0) {
-                writer.writeFloat8(getSum() / count);
-            } else {
-                writer.writeNull();
-            }
-        }
+    @Override
+    protected FieldVector evaluateImpl() throws ComputeException {
+      if (count == 0) {
+        FieldVector result = field.createVector(allocator);
+        result.setNull(0); // If there's no data, the average is null
+        return result;
+      }
+      FieldVector result = field.createVector(allocator);
+      writeValue(result.getMinorType().getNewFieldWriter(result));
+      return result;
     }
 
-    protected static class IntAvgState extends AvgState {
+    protected abstract double getSum();
 
-        private long sum = 0;
+    protected void writeValue(FieldWriter writer) {
+      if (count > 0) {
+        writer.writeFloat8(getSum() / count);
+      } else {
+        writer.writeNull();
+      }
+    }
+  }
 
-        public IntAvgState(BufferAllocator allocator, Field field, PhysicalAvg accumulator) {
-            super(allocator, field, accumulator);
-        }
+  protected static class IntAvgState extends AvgState {
 
-        @Override
-        protected void accumulateImpl(ValueVector vector) throws ComputeException {
-            IntVector intVector = (IntVector) vector;
-            for (int i = 0; i < intVector.getValueCount(); i++) {
-                if (!intVector.isNull(i)) {
-                    sum += intVector.get(i);
-                    count++;
-                }
-            }
-        }
+    private long sum = 0;
 
-        @Override
-        protected double getSum() {
-            return sum;
-        }
+    public IntAvgState(BufferAllocator allocator, Field field, PhysicalAvg accumulator) {
+      super(allocator, field, accumulator);
     }
 
-    protected static class BigIntAvgState extends AvgState {
-
-        private long sum = 0;
-
-        public BigIntAvgState(BufferAllocator allocator, Field field, PhysicalAvg accumulator) {
-            super(allocator, field, accumulator);
+    @Override
+    protected void accumulateImpl(ValueVector vector) throws ComputeException {
+      IntVector intVector = (IntVector) vector;
+      for (int i = 0; i < intVector.getValueCount(); i++) {
+        if (!intVector.isNull(i)) {
+          sum += intVector.get(i);
+          count++;
         }
-
-        @Override
-        protected void accumulateImpl(ValueVector vector) throws ComputeException {
-            BigIntVector bigIntVector = (BigIntVector) vector;
-            for (int i = 0; i < bigIntVector.getValueCount(); i++) {
-                if (!bigIntVector.isNull(i)) {
-                    sum += bigIntVector.get(i);
-                    count++;
-                }
-            }
-        }
-
-        @Override
-        protected double getSum() {
-            return sum;
-        }
+      }
     }
 
-    protected static class Float4AvgState extends AvgState {
+    @Override
+    protected double getSum() {
+      return sum;
+    }
+  }
 
-        private double sum = 0;
+  protected static class BigIntAvgState extends AvgState {
 
-        public Float4AvgState(BufferAllocator allocator, Field field, PhysicalAvg accumulator) {
-            super(allocator, field, accumulator);
-        }
+    private long sum = 0;
 
-        @Override
-        protected void accumulateImpl(ValueVector vector) throws ComputeException {
-            Float4Vector float4Vector = (Float4Vector) vector;
-            for (int i = 0; i < float4Vector.getValueCount(); i++) {
-                if (!float4Vector.isNull(i)) {
-                    sum += float4Vector.get(i);
-                    count++;
-                }
-            }
-        }
-
-        @Override
-        protected double getSum() {
-            return sum;
-        }
+    public BigIntAvgState(BufferAllocator allocator, Field field, PhysicalAvg accumulator) {
+      super(allocator, field, accumulator);
     }
 
-    protected static class Float8AvgState extends AvgState {
-
-        private double sum = 0;
-
-        public Float8AvgState(BufferAllocator allocator, Field field, PhysicalAvg accumulator) {
-            super(allocator, field, accumulator);
+    @Override
+    protected void accumulateImpl(ValueVector vector) throws ComputeException {
+      BigIntVector bigIntVector = (BigIntVector) vector;
+      for (int i = 0; i < bigIntVector.getValueCount(); i++) {
+        if (!bigIntVector.isNull(i)) {
+          sum += bigIntVector.get(i);
+          count++;
         }
-
-        @Override
-        protected void accumulateImpl(ValueVector vector) throws ComputeException {
-            Float8Vector float8Vector = (Float8Vector) vector;
-            for (int i = 0; i < float8Vector.getValueCount(); i++) {
-                if (!float8Vector.isNull(i)) {
-                    sum += float8Vector.get(i);
-                    count++;
-                }
-            }
-        }
-
-        @Override
-        protected double getSum() {
-            return sum;
-        }
+      }
     }
+
+    @Override
+    protected double getSum() {
+      return sum;
+    }
+  }
+
+  protected static class Float4AvgState extends AvgState {
+
+    private double sum = 0;
+
+    public Float4AvgState(BufferAllocator allocator, Field field, PhysicalAvg accumulator) {
+      super(allocator, field, accumulator);
+    }
+
+    @Override
+    protected void accumulateImpl(ValueVector vector) throws ComputeException {
+      Float4Vector float4Vector = (Float4Vector) vector;
+      for (int i = 0; i < float4Vector.getValueCount(); i++) {
+        if (!float4Vector.isNull(i)) {
+          sum += float4Vector.get(i);
+          count++;
+        }
+      }
+    }
+
+    @Override
+    protected double getSum() {
+      return sum;
+    }
+  }
+
+  protected static class Float8AvgState extends AvgState {
+
+    private double sum = 0;
+
+    public Float8AvgState(BufferAllocator allocator, Field field, PhysicalAvg accumulator) {
+      super(allocator, field, accumulator);
+    }
+
+    @Override
+    protected void accumulateImpl(ValueVector vector) throws ComputeException {
+      Float8Vector float8Vector = (Float8Vector) vector;
+      for (int i = 0; i < float8Vector.getValueCount(); i++) {
+        if (!float8Vector.isNull(i)) {
+          sum += float8Vector.get(i);
+          count++;
+        }
+      }
+    }
+
+    @Override
+    protected double getSum() {
+      return sum;
+    }
+  }
 }
--- core/src/main/java/cn/edu/tsinghua/iginx/engine/physical/memory/execute/compute/accumulate/unary/PhysicalMin.java
+++ core/src/main/java/cn/edu/tsinghua/iginx/engine/physical/memory/execute/compute/accumulate/unary/PhysicalMin.java
@@ -2,6 +2,7 @@ package cn.edu.tsinghua.iginx.engine.physical.memory.execute.compute.accumulate.
 
 import cn.edu.tsinghua.iginx.engine.physical.memory.execute.compute.util.ComputeException;
 import cn.edu.tsinghua.iginx.engine.physical.memory.execute.compute.util.NotAllowArgumentTypeException;
+import org.apache.arrow.memory.ArrowBuf;
 import org.apache.arrow.memory.BufferAllocator;
 import org.apache.arrow.vector.*;
 import org.apache.arrow.vector.complex.writer.FieldWriter;
@@ -10,147 +11,182 @@ import org.apache.arrow.vector.types.pojo.Field;
 
 public class PhysicalMin extends UnaryAccumulator {
 
-    public static final String NAME = "min";
+  public static final String NAME = "min";
 
-    public PhysicalMin() {
-        super(NAME);
+  public PhysicalMin() {
+    super(NAME);
+  }
+
+  @Override
+  protected UnaryState initializeImpl(BufferAllocator allocator, Field field)
+      throws ComputeException {
+    Types.MinorType minorType = Types.getMinorTypeForArrowType(field.getType());
+    switch (minorType) {
+      case INT:
+        return new IntMinState(allocator, field, this);
+      case BIGINT:
+        return new BigIntMinState(allocator, field, this);
+      case FLOAT4:
+        return new Float4MinState(allocator, field, this);
+      case FLOAT8:
+        return new Float8MinState(allocator, field, this);
+      default:
+        throw new NotAllowArgumentTypeException(this, 0, minorType);
+    }
+  }
+
+  protected abstract static class MinState extends UnaryState {
+
+    protected MinState(BufferAllocator allocator, Field field, PhysicalMin accumulator) {
+      super(allocator, field, accumulator);
     }
 
     @Override
-    protected UnaryState initializeImpl(BufferAllocator allocator, Field field)
-        throws ComputeException {
-        Types.MinorType minorType = Types.getMinorTypeForArrowType(field.getType());
-        switch (minorType) {
-            case INT:
-                return new IntMinState(allocator, field, this);
-            case BIGINT:
-                return new BigIntMinState(allocator, field, this);
-            case FLOAT4:
-                return new Float4MinState(allocator, field, this);
-            case FLOAT8:
-                return new Float8MinState(allocator, field, this);
-            default:
-                throw new NotAllowArgumentTypeException(this, 0, minorType);
-        }
+    public boolean needMoreData() throws ComputeException {
+      return true;
     }
 
-    protected abstract static class MinState extends UnaryState {
+    @Override
+    public void close() throws ComputeException {}
 
-        protected MinState(BufferAllocator allocator, Field field, PhysicalMin accumulator) {
-            super(allocator, field, accumulator);
-        }
-
-        @Override
-        public boolean needMoreData() throws ComputeException {
-            return true;
-        }
-
-        @Override
-        public void close() throws ComputeException {}
-
-        @Override
-        protected FieldVector evaluateImpl() throws ComputeException {
-            FieldVector result = field.createVector(allocator);
-            writeValue(result.getMinorType().getNewFieldWriter(result));
-            return result;
-        }
-
-        protected abstract void writeValue(FieldWriter writer);
+    @Override
+    protected FieldVector evaluateImpl() throws ComputeException {
+      FieldVector result = field.createVector(allocator);
+      writeValue(result.getMinorType().getNewFieldWriter(result));
+      return result;
     }
 
-    protected static class IntMinState extends MinState {
+    protected abstract void writeValue(FieldWriter writer);
+  }
 
-        private int min = Integer.MAX_VALUE;
+  protected static class IntMinState extends MinState {
 
-        public IntMinState(BufferAllocator allocator, Field field, PhysicalMin accumulator) {
-            super(allocator, field, accumulator);
-        }
+    private int min = Integer.MAX_VALUE;
 
-        @Override
-        protected void accumulateImpl(ValueVector vector) throws ComputeException {
-            IntVector intVector = (IntVector) vector;
-            for (int i = 0; i < intVector.getValueCount(); i++) {
-                if (!intVector.isNull(i)) {
-                    min = Math.min(min, intVector.get(i));
-                }
-            }
-        }
-
-        @Override
-        protected void writeValue(FieldWriter writer) {
-            writer.writeInt(min);
-        }
+    public IntMinState(BufferAllocator allocator, Field field, PhysicalMin accumulator) {
+      super(allocator, field, accumulator);
     }
 
-    protected static class BigIntMinState extends MinState {
-
-        private long min = Long.MAX_VALUE;
-
-        public BigIntMinState(BufferAllocator allocator, Field field, PhysicalMin accumulator) {
-            super(allocator, field, accumulator);
+    @Override
+    protected void accumulateImpl(ValueVector vector) throws ComputeException {
+      IntVector intVector = (IntVector) vector;
+      for (int i = 0; i < intVector.getValueCount(); i++) {
+        if (!intVector.isNull(i)) {
+          min = Math.min(min, intVector.get(i));
         }
-
-        @Override
-        protected void accumulateImpl(ValueVector vector) throws ComputeException {
-            BigIntVector bigIntVector = (BigIntVector) vector;
-            for (int i = 0; i < bigIntVector.getValueCount(); i++) {
-                if (!bigIntVector.isNull(i)) {
-                    min = Math.min(min, bigIntVector.get(i));
-                }
-            }
-        }
-
-        @Override
-        protected void writeValue(FieldWriter writer) {
-            writer.writeBigInt(min);
-        }
+      }
     }
 
-    protected static class Float4MinState extends MinState {
+    @Override
+    protected void writeValue(FieldWriter writer) {
+      writer.writeInt(min);
+    }
+  }
 
-        private float min = Float.POSITIVE_INFINITY;
+  protected static class BigIntMinState extends MinState {
 
-        public Float4MinState(BufferAllocator allocator, Field field, PhysicalMin accumulator) {
-            super(allocator, field, accumulator);
-        }
+    private long min = Long.MAX_VALUE;
 
-        @Override
-        protected void accumulateImpl(ValueVector vector) throws ComputeException {
-            Float4Vector float4Vector = (Float4Vector) vector;
-            for (int i = 0; i < float4Vector.getValueCount(); i++) {
-                if (!float4Vector.isNull(i)) {
-                    min = Math.min(min, float4Vector.get(i));
-                }
-            }
-        }
-
-        @Override
-        protected void writeValue(FieldWriter writer) {
-            writer.writeFloat4(min);
-        }
+    public BigIntMinState(BufferAllocator allocator, Field field, PhysicalMin accumulator) {
+      super(allocator, field, accumulator);
     }
 
-    protected static class Float8MinState extends MinState {
-
-        private double min = Double.POSITIVE_INFINITY;
-
-        public Float8MinState(BufferAllocator allocator, Field field, PhysicalMin accumulator) {
-            super(allocator, field, accumulator);
+    @Override
+    protected void accumulateImpl(ValueVector vector) throws ComputeException {
+      BigIntVector bigIntVector = (BigIntVector) vector;
+      for (int i = 0; i < bigIntVector.getValueCount(); i++) {
+        if (!bigIntVector.isNull(i)) {
+          min = Math.min(min, bigIntVector.get(i));
         }
-
-        @Override
-        protected void accumulateImpl(ValueVector vector) throws ComputeException {
-            Float8Vector float8Vector = (Float8Vector) vector;
-            for (int i = 0; i < float8Vector.getValueCount(); i++) {
-                if (!float8Vector.isNull(i)) {
-                    min = Math.min(min, float8Vector.get(i));
-                }
-            }
-        }
-
-        @Override
-        protected void writeValue(FieldWriter writer) {
-            writer.writeFloat8(min);
-        }
+      }
     }
+
+    @Override
+    protected void writeValue(FieldWriter writer) {
+      writer.writeBigInt(min);
+    }
+  }
+
+  protected static class Float4MinState extends MinState {
+
+    private float min = Float.POSITIVE_INFINITY;
+
+    public Float4MinState(BufferAllocator allocator, Field field, PhysicalMin accumulator) {
+      super(allocator, field, accumulator);
+    }
+
+    @Override
+    protected void accumulateImpl(ValueVector vector) throws ComputeException {
+      Float4Vector float4Vector = (Float4Vector) vector;
+      for (int i = 0; i < float4Vector.getValueCount(); i++) {
+        if (!float4Vector.isNull(i)) {
+          min = Math.min(min, float4Vector.get(i));
+        }
+      }
+    }
+
+    @Override
+    protected void writeValue(FieldWriter writer) {
+      writer.writeFloat4(min);
+    }
+  }
+
+  protected static class Float8MinState extends MinState {
+
+    private double min = Double.POSITIVE_INFINITY;
+
+    public Float8MinState(BufferAllocator allocator, Field field, PhysicalMin accumulator) {
+      super(allocator, field, accumulator);
+    }
+
+    @Override
+    protected void accumulateImpl(ValueVector vector) throws ComputeException {
+      Float8Vector float8Vector = (Float8Vector) vector;
+      for (int i = 0; i < float8Vector.getValueCount(); i++) {
+        if (!float8Vector.isNull(i)) {
+          min = Math.min(min, float8Vector.get(i));
+        }
+      }
+    }
+
+    @Override
+    protected void writeValue(FieldWriter writer) {
+      writer.writeFloat8(min);
+    }
+  }
+
+  protected static class StringMaxState extends MinState {
+
+    private ArrowBuf min;
+    private byte[] minStr = null;
+
+    public StringMaxState(BufferAllocator allocator, Field field, PhysicalMin accumulator) {
+      super(allocator, field, accumulator);
+    }
+
+    @Override
+    protected void accumulateImpl(ValueVector vector) throws ComputeException {
+      VarCharVector varCharVector = (VarCharVector) vector;
+      for (int i = 0; i < varCharVector.getValueCount(); i++) {
+        if (!varCharVector.isNull(i)) {
+          byte[] current = varCharVector.get(i);
+          if (minStr == null || new String(current).compareTo(new String(minStr)) > 0) { // 使用 compareTo 进行比较
+            minStr = current;
+          }
+        }
+      }
+    }
+
+    @Override
+    protected void writeValue(FieldWriter writer) {
+      if (minStr != null) {
+        int length = minStr.length;
+        min = allocator.buffer(length);
+        min.setBytes(0, minStr, 0, length);
+        writer.writeVarChar(0, (int) min.writerIndex(), min);
+      } else {
+        writer.writeNull();
+      }
+    }
+  }
 }
